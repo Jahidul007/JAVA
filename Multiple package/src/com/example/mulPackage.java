d7747e6a14391b98a860f68bce126227359783b1
Jahidul Islam <jahidul479@gmail.com>
2018-01-25 19:33:45 +0600

    initial commit

---
+++ Multiple package/src/com/example/mulPackage.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import newPackage.newClass;
+
+public class mulPackage {
+	 public static void main (String[] args )
+	   {
+		   System.out.println(newClass.GETTING);
+	   }
+
+}
---
+++ Multiple package/src/newPackage/newClass.java
@@ -0,0 +1,10 @@
+package newPackage;
+
+public class newClass {
+	private newClass()
+	{
+		
+	}
+
+	public static final String GETTING = " Here! Multiple appp";
+}
